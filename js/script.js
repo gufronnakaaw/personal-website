602d2e7b1630cd3ae93832c22a2fa6474f24049f
gufronnakaaw <gufronnakaaw@gmail.com>
2021-01-27 21:07:11 +0700

    create sticky navbar

--- js/script.js
+++ js/script.js
@@ -1,8 +1,10 @@
 const btnDownload = document.querySelector('.download-resume');
 
 window.onload = function() {
-    document.querySelector('.overlay').style.display = "none";
-    document.querySelector('.loading-spinner').style.display = "none";
+    setInterval(() => {
+        document.querySelector('.overlay').style.display = "none";
+        document.querySelector('.loading-spinner').style.display = "none";
+    }, 500);
 }
 
 btnDownload.addEventListener('click', () => {
@@ -15,4 +17,12 @@ btnDownload.addEventListener('click', () => {
         timer: 1500,
         timerProgressBar: true
     });
+});
+
+
+const navbar = document.querySelector(".navbar");
+const sticky = navbar.offsetTop;
+
+window.addEventListener('scroll', () => {
+    (window.pageYOffset >= sticky) ? navbar.classList.add("navbar-fixed") : navbar.classList.remove("navbar-fixed")
 });
