ea03195530141d50e5a944f05c15795c84e98e62
gufronnakaaw <gufronnakaaw@gmail.com>
2023-03-07 19:55:09 +0700

    update: delete social-media links and change contact button

--- js/script.js
+++ js/script.js
@@ -4,21 +4,21 @@ const overlay = document.querySelector('.overlay');
 
 // for loading
 window.onload = function () {
-    // setInterval(() => {
-    //     document.querySelector('.overlay').style.display = 'none';
-    //     document.querySelector('.loading-spinner').style.display = 'none';
-    // }, 500);
+  // setInterval(() => {
+  //     document.querySelector('.overlay').style.display = 'none';
+  //     document.querySelector('.loading-spinner').style.display = 'none';
+  // }, 500);
 };
 
 // hamburger section
 hamburger.addEventListener('click', function () {
-    sidebar.classList.add('sidebar-active');
-    overlay.style.display = 'block';
+  sidebar.classList.add('sidebar-active');
+  overlay.style.display = 'block';
 });
 
 overlay.addEventListener('click', function () {
-    sidebar.classList.remove('sidebar-active');
-    this.style.display = 'none';
+  sidebar.classList.remove('sidebar-active');
+  this.style.display = 'none';
 });
 
 const about = document.querySelector('#about');
@@ -26,32 +26,32 @@ const skills = document.querySelector('#skills');
 const personalProject = document.querySelector('#personal-project');
 
 window.onscroll = function () {
-    if (Math.round(window.pageYOffset) < about.offsetTop) {
-        hamburger.style.display = 'none';
-    } else if (
-        Math.round(window.pageYOffset) >= about.offsetTop &&
-        Math.round(window.pageYOffset) <= skills.offsetTop
-    ) {
-        hamburger.style.display = 'block';
-        hamburger.style.color = 'white';
-    } else if (
-        Math.round(window.pageYOffset) >= skills.offsetTop &&
-        Math.round(window.pageYOffset) <= personalProject.offsetTop
-    ) {
-        hamburger.style.display = 'block';
-        hamburger.style.color = 'black';
-    } else if (Math.round(window.pageYOffset) >= personalProject.offsetTop) {
-        hamburger.style.display = 'block';
-        hamburger.style.color = 'white';
-    }
+  if (Math.round(window.pageYOffset) < about.offsetTop) {
+    hamburger.style.display = 'none';
+  } else if (
+    Math.round(window.pageYOffset) >= about.offsetTop &&
+    Math.round(window.pageYOffset) <= skills.offsetTop
+  ) {
+    hamburger.style.display = 'block';
+    hamburger.style.color = 'white';
+  } else if (
+    Math.round(window.pageYOffset) >= skills.offsetTop &&
+    Math.round(window.pageYOffset) <= personalProject.offsetTop
+  ) {
+    hamburger.style.display = 'block';
+    hamburger.style.color = 'black';
+  } else if (Math.round(window.pageYOffset) >= personalProject.offsetTop) {
+    hamburger.style.display = 'block';
+    hamburger.style.color = 'white';
+  }
 };
 
 // typed js section
 const options = {
-    strings: ['Back-end Web Developer'],
-    typeSpeed: 77,
-    showCursor: false,
-    backSpeed: 50,
+  strings: ['Back-end Web Developer'],
+  typeSpeed: 77,
+  showCursor: false,
+  backSpeed: 50,
 };
 
 new Typed('#role', options);
@@ -61,25 +61,25 @@ AOS.init();
 
 // wow section
 new WOW().init({
-    mobile: true,
+  mobile: true,
 });
 
-// contact button section
-const contactButton = document.querySelectorAll('.contact-button');
+// connect button section
+const connectButton = document.querySelectorAll('.connect-button');
 
-contactButton.forEach((button) => {
-    button.addEventListener('click', function () {
-        window.location.href = `mailto: gufronnakaaw@gmail.com`;
-    });
+connectButton.forEach((button) => {
+  button.addEventListener('click', function () {
+    window.location.href = `https://links.wildandev.com`;
+  });
 });
 
 // e-certificate section
 const certificate = document.querySelectorAll('.e-certificate');
 
 certificate.forEach((cert) => {
-    cert.setAttribute(
-        'href',
-        'https://drive.google.com/drive/folders/1XJ-UMKjM37lEzyC6dSOnUzGvYT3aMNKv?usp=sharing'
-    );
-    cert.setAttribute('target', '_blank');
+  cert.setAttribute(
+    'href',
+    'https://drive.google.com/drive/folders/1XJ-UMKjM37lEzyC6dSOnUzGvYT3aMNKv?usp=sharing'
+  );
+  cert.setAttribute('target', '_blank');
 });
